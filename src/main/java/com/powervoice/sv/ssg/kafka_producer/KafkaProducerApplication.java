489aad5af781feb5cec3865bdc8bf6976b585862
Yuchan-Kim <yuchankim0906@gmail.com>
2025-07-21 09:13:53 +0900

    first commit

---
+++ src/main/java/com/powervoice/sv/ssg/kafka_producer/KafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.powervoice.sv.ssg.kafka_producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducerApplication.class, args);
+    }
+
+}
